7d37f767808dab5ffcfef3faf92e2bcde6ec0b6d
Andrey Solodovnikov <a.solodovnikov@office.ngs.ru>
2019-12-09 18:48:41 +0300

    adds stage build urls

--- src/getFunctionalUrls.js
+++ src/getFunctionalUrls.js
@@ -3,13 +3,15 @@ const buildPurpose = process.env.VUE_APP_BUILD_PURPOSE;
 const apiUrlMap = {
   dev: 'https://p1payapi.tst.protocol.one',
   test: 'https://p1payapi.tst.protocol.one',
+  stage: 'https://api.stg.pay.super.com',
   release: 'https://api.pay.super.com',
 };
 
 const formUrlMap = {
   dev: 'http://localhost:8080/order',
   test: 'https://paysupermgmt.tst.protocol.one/order',
-  release: 'https://order.pay.super.com/',
+  stage: 'https://order.stg.pay.super.com',
+  release: 'https://order.pay.super.com',
 };
 
 export default function getFunctionalUrls({ apiUrl, formUrl }) {
