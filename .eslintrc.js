d83a32f351ce7b2d1a6b46f270837189cd34658b
Andrey Solodovnikov <raiky@yandex.ru>
2021-01-22 12:46:26 +0300

    no-console rule change

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
     },
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-param-reassign': ['error', { ignorePropertyModificationsFor: ['state'] }],
   },
